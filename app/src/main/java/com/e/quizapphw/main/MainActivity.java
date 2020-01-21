96220c8d9591eee927b5b21528856e680c012c4b
Malikovich <md.malikovich@gmail.com>
2020-01-21 20:12:22 +0600

    +Splash screen

--- app/src/main/java/com/e/quizapphw/main/MainActivity.java
+++ app/src/main/java/com/e/quizapphw/main/MainActivity.java
@@ -1,8 +1,12 @@
-package com.e.quizapphw;
+package com.e.quizapphw.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.e.quizapphw.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, MainActivity.class));
     }
 }
