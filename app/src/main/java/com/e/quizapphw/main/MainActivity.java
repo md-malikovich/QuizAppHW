bb44b4cb8ad4ab6bbf1294e00ab50dfbfcd4b328
Malikovich <md.malikovich@gmail.com>
2020-01-21 22:24:01 +0600

    +all that we did in class +history package

--- app/src/main/java/com/e/quizapphw/main/MainActivity.java
+++ app/src/main/java/com/e/quizapphw/main/MainActivity.java
@@ -1,19 +1,69 @@
 package com.e.quizapphw.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.e.quizapphw.R;
+import com.e.quizapphw.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private MainViewModel mViewModel;
+
+    private ViewPager mViewPager;
+    private MainPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mViewModel = ViewModelProviders.of(this) //
+//                .get(MainViewModel.class);
+
+        mViewPager = findViewById(R.id.main_view_pager);
+        mAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        //mViewPager.setOffscreenPageLimit(100); - для того чтобы прогрузить сразу все фрагменты
+
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(android.R.id.content, new MainFragment())
+//                .commit();
+    }
+
+    private class MainPagerAdapter extends FragmentPagerAdapter {
+
+        public MainPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment;
+            switch (position) {
+                case 0:
+                    fragment = MainFragment.newInstance();
+                    break;
+                default:
+                    fragment = SettingsFragment.newInstance();
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 
     public static void start(Context context) {
