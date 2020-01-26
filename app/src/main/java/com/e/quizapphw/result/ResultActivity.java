6f363e8c37dc9512bc9c82e9bceb4548d354a6dd
Malikovich <md.malikovich@gmail.com>
2020-01-26 20:57:11 +0600

    + Добавить Result - done + History - done

---
+++ app/src/main/java/com/e/quizapphw/result/ResultActivity.java
@@ -0,0 +1,16 @@
+package com.e.quizapphw.result;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.e.quizapphw.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
