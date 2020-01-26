731a27f5b8c9911c20c46e43786d7ac90750f39e
Malikovich <md.malikovich@gmail.com>
2020-01-26 21:39:45 +0600

    + переход из MainFragment в ResultActivity (нажатие на img) и обратно (btn Finish)

--- app/src/main/java/com/e/quizapphw/main/MainFragment.java
+++ app/src/main/java/com/e/quizapphw/main/MainFragment.java
@@ -2,18 +2,28 @@ package com.e.quizapphw.main;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.e.quizapphw.R;
 import com.e.quizapphw.core.CoreFragment;
+import com.e.quizapphw.result.ResultActivity;
+
 import java.util.Objects;
 
 public class MainFragment extends CoreFragment {
 
     private MainViewModel mViewModel;
+    private ImageView skip;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -41,5 +51,16 @@ public class MainFragment extends CoreFragment {
                 Log.d("ololo", s);
             }
         });
+
+        skip = (ImageView) getActivity().findViewById(R.id.imgQuiz);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/e/quizapphw/result/ResultActivity.java
+++ app/src/main/java/com/e/quizapphw/result/ResultActivity.java
@@ -2,15 +2,31 @@ package com.e.quizapphw.result;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.e.quizapphw.R;
+import com.e.quizapphw.main.MainActivity;
+import com.e.quizapphw.main.MainFragment;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private Button finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        finish = findViewById(R.id.btnFinish);
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
